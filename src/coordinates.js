b41527d165a29a821864dc8990261939b1cae457
Torjus Iveland <torjusti@gmail.com>
2017-08-04 16:59:38 +0200

    docs: Comments above coordinate definitions

--- src/coordinates.js
+++ src/coordinates.js
@@ -1,5 +1,9 @@
 import { factorial, choose } from './tools';
 
+/**
+ * In-place rotation of the subarray determined by the two
+ * indexes left and right to the left by one.
+ */
 const rotateLeft = (pieces, left, right) => {
   const original = pieces[left];
 
@@ -10,6 +14,10 @@ const rotateLeft = (pieces, left, right) => {
   pieces[right] = original;
 };
 
+/**
+ * In-place rotation of the subarray determined by the two
+ * indexes left and right to the right by one.
+ */
 const rotateRight = (pieces, left, right) => {
   const original = pieces[right];
 
@@ -20,15 +28,32 @@ const rotateRight = (pieces, left, right) => {
   pieces[left] = original;
 };
 
+/**
+ * Bijection which encodes a given orientation vector to an unique index.
+ * The flip count is the number of ways in which a single piece in the
+ * orientation vector may be oriented. For edges, this number is 2 flips,
+ * and for corners there are 3 possible twists. Thus, edges are encoded
+ * using a binary number system, and corners using a trinary number system.
+ */
 export const getIndexFromOrientation = (pieces, flipCount) => {
   let sum = 0;
 
+  // Note that we do not include the last element in the vector here.
+  // This is because the orientation of the last piece if determined
+  // by the orientation of all the other pieces - when you rotate any
+  // axis on the cube, only an even number of pieces is affected.
   for (let i = 0; i < pieces.length - 1; i += 1) {
     sum = flipCount * sum + pieces[i];
   }
 
   return sum;
 };
+
+/**
+ * Returns the original orientation vector given the number which
+ * describes it, the number of pieces in the vector, and the number
+ * of ways in which an individual piece may be oriented.
+ */
 
 export const getOrientationFromIndex = (index, numPieces, numFlips) => {
   const orientation = [];
@@ -42,11 +67,22 @@ export const getOrientationFromIndex = (index, numPieces, numFlips) => {
     parity += ori;
   }
 
+  // Restore the last piece based on the orientation of the other pieces.
   orientation[numPieces - 1] = (numFlips - parity % numFlips) % numFlips;
 
   return orientation;
 };
 
+/**
+ * Each move on a cube perform an even number of swaps when considering
+ * both edges and corner pieces at the same time. Thus, only half of all
+ * cube states are reachable using legal moves. This also implies that
+ * for a cube to be solvable, the parity of both corners and edges must
+ * both be either even or odd. We use this to verify that a cube is
+ * solvable when generating random state scrambles, and also to
+ * describe the overall cube permutation using only 10 edges, 6 corners
+ * and the parity of either the corners or the edges.
+ */
 export const getParity = (pieces) => {
   let sum = 0;
 
@@ -61,11 +97,25 @@ export const getParity = (pieces) => {
   return sum % 2;
 };
 
+/**
+ * Encodes the permutation of the affected pieces within the entire
+ * permutation vector, by encoding both their position and then the
+ * permutation of the affected pieces within the permutation vector
+ * using a variable-base number system. If reversed is true, the
+ * values are assigned right-to-left. This is used in the Kociemba
+ * solver, so that 0 is used as the solved coordinate for the move
+ * table describing the UD-slice edges.
+ */
 export const getIndexFromPermutation = (pieces, affected, reversed = false) => {
   let position = 0, k = 1, offset = pieces.length - 1;
 
+  // Store the permutation of the subarray containing
+  // only the affected pieces.
   const edges = [];
 
+  // Encode the position of the affected pieces in a number
+  // from 0 up to n choose k, where n is the number of pieces
+  // in the permutation vector and k is the number of affected pieces.
   if (reversed) {
     for (let n = pieces.length - 1; n >= 0; n -= 1) {
       if (affected.indexOf(pieces[n]) >= 0) {
@@ -88,6 +138,8 @@ export const getIndexFromPermutation = (pieces, affected, reversed = false) => {
 
   let permutation = 0;
 
+  // Encode the position of the subarray as a number from 0 and up
+  // to n factorial, where n is the number of affected pieces.
   for (let i = edges.length - 1; i > 0; i -= 1) {
     let s = 0;
 
@@ -99,9 +151,16 @@ export const getIndexFromPermutation = (pieces, affected, reversed = false) => {
     permutation = (i + 1) * permutation + s;
   }
 
+  // Encode both the position and the permutation
+  // as a number using a variable base.
   return factorial(affected.length) * position + permutation;
 };
 
+/**
+ * Restores the permutation described by an index, number of affected
+ * pieces and the permutation vector size. If reversed is true, the
+ * indexes have been assigned right-to-left.
+ */
 export const getPermutationFromIndex = (index, affected, size, reversed = false) => {
   const base = factorial(affected.length);
 
