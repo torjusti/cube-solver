dd449ad91136b4993f451a79c38fe8544b2b8024
Torjus Iveland <torjusti@gmail.com>
2017-08-07 21:20:11 +0200

    Remove test code

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import kociemba from './kociemba';
 import getRandomScramble from './scramble';
 import { crossSolver, firstBlockSolver, EOLineSolver } from './solvers';
 
-console.time('init+solve');
-getRandomScramble();
-console.timeEnd('init+solve');
-
 export default {
   solve: kociemba,
   getRandomScramble,
