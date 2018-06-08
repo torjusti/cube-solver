06ef31af02bc577b10968a9eee307a9f7a8f4c90
Torjus Iveland <torjusti@gmail.com>
2018-06-08 14:23:23 +0200

    feat: Make internal API public

--- src/cube.js
+++ src/cube.js
@@ -133,3 +133,7 @@ export const doAlgorithm = (algorithm, cube = identity) => {
 
   return { ep, eo, cp, co };
 };
+
+
+// All legal moves.
+export const allMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+// Import solvers.
 import kociemba from './kociemba';
 import getRandomScramble from './scramble';
 import { crossSolver, firstBlockSolver, EOLineSolver } from './solvers';
+
+// Import utility modules.
+import * as algorithms from './algorithms';
+import * as coordinates from './coordinates';
+import * as cube from './cube';
+import * as MoveTable from './MoveTable';
+import PruningTable from './PruningTable';
+import Search from './Search';
+import * as tools from './tools';
 
 export default {
   solve: kociemba,
@@ -8,4 +18,12 @@ export default {
   crossSolver,
   firstBlockSolver,
   EOLineSolver,
+
+  algorithms,
+  coordinates,
+  cube,
+  MoveTable,
+  PruningTable,
+  Search,
+  tools,
 };
