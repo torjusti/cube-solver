27c65d1553fccebac24a576b3aaf112b969e5b8a
Torjus Iveland <torjusti@gmail.com>
2018-07-16 21:53:20 +0200

    Add corners scrambler

---
+++ src/scramblers/corners.js
@@ -0,0 +1,10 @@
+import getScrambleForPieces from './scramblePieces';
+import { Edges } from '../cube';
+
+const getScramble = () => getScrambleForPieces(
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+
+  [],
+);
+
+export default getScramble;
