377b9242749b9b60216769151873ce8af0e17540
Torjus Iveland <torjusti@gmail.com>
2018-07-17 18:38:26 +0200

    Add LSLL scrambler

---
+++ src/scramblers/lsll.js
@@ -0,0 +1,11 @@
+import getScrambleForPieces from './scramblePieces';
+import { Edges, Corners } from '../cube';
+
+const getScramble = () =>
+  getScrambleForPieces(
+    [Edges.UR, Edges.UF, Edges.UL, Edges.UB, Edges.FR],
+
+    [Corners.URF, Corners.UFL, Corners.ULB, Corners.UBR, Corners.DFR],
+  );
+
+export default getScramble;
