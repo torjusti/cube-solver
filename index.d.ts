9a42d86653b500771240a25390da01ba63baed94
Torjus Iveland <torjusti@gmail.com>
2019-12-25 02:19:53 +0100

    Correct type definitions

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,10 @@ type Scrambler = '3x3' | '2gll' | 'cmll' | 'corners' | 'edges' | 'lse' | 'lsll' 
 type Solver = 'kociemba' | 'cross' | 'eoline' | 'fb' | 'xcross';
 
 declare module 'cube-solver' {
-  // Main method for generating scrambls.
+  // Main method for generating scrambles.
   export const scramble: (scrambler: Scrambler) => string;
   // Solve specific subset of a given cube.
   export const solve: (scramble: string, type: Solver) => string;
+  // Initialize given solver.
+  export const initialize: (solver: Solver) => void;
 };
