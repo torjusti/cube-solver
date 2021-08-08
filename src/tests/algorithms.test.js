fdc53f8152a30213d5cc4f9b71668e0ef0c5800f
Torjus Iveland <torjusti@gmail.com>
2021-08-08 12:14:47 +0200

    fix: Prevent powers being added multiple times when mapping wide moves with inverted rotations in their description

--- src/tests/algorithms.test.js
+++ src/tests/algorithms.test.js
@@ -1,7 +1,7 @@
 import * as algorithms from '../algorithms';
 
 describe('algorithms', () => {
-  it('allow parsing invalid algorithms', () => {
+  it('should not allow parsing invalid algorithms', () => {
     expect(() => {
       algorithms.parseAlgorithm("R U R' U' 1337 R' F R F'");
     }).toThrowError();
@@ -15,5 +15,9 @@ describe('algorithms', () => {
   it('should strip rotations from algorithms correctly', () => {
     expect(algorithms.parseAlgorithm("x R u R' u' y' z2 R' f R f'"))
       .toMatchSnapshot();
+
+    // Regression: Wide moves described using inverted rotations would
+    // add powers multiple times to a single move, crashing the parser. 
+    expect(algorithms.parseAlgorithm("f' r' u' b' l' d'")).toMatchSnapshot();
   });
 });
